879844e1e198aed6bba65a4b175eb495c8b07997
Ocean Barras <ocean.barras@hotmail.com>
2025-10-21 12:32:57 -0400

    add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="">
+        <h1>Welcome to Financial Flow</h1>
+        <p>Your personal finance management tool.</p>
+        <button>Login</button>
+    </div>
+  )
+}
