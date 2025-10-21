e305665df57a6dfc0e77cd05d8d14e502bd12ad9
Ocean Barras <ocean.barras@hotmail.com>
2025-10-21 17:56:15 -0400

    setup forced auth in the main app

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,24 @@ import './index.css'
 import App from './App'
 import Home from './pages/Home'
 import About from './pages/About'
+import { MsalProvider } from "@azure/msal-react";
+import { msalInstance } from "./components/login/AuthConfig";
+import RequireAuth from './components/login/RequiredAuth'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
+      <MsalProvider instance={msalInstance}>
+        <RequireAuth >
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+        </RequireAuth>
+      </MsalProvider>
     </BrowserRouter>
   </StrictMode>,
 )
