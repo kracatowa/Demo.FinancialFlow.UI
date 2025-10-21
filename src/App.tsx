8660e3d2701faf6d44337097493687922ca60f77
Ocean Barras <ocean.barras@hotmail.com>
2025-10-21 12:33:12 -0400

    create routing and main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer'
+import TopNavbar from './components/TopNavbar'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-
-
   return (
     <>
-
+      <TopNavbar />
+      <div className="main-content">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   )
 }
