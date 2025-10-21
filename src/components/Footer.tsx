b5cba0520cc9e4eb410c005cba6f21d9862d81ba
Ocean Barras <ocean.barras@hotmail.com>
2025-10-21 12:25:36 -0400

    add footer page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2023 Financial Flow. All rights reserved.</p>
+      <p>Privacy Policy | Terms of Service</p>
+      <p>Contact Us</p>
+    </div>
+  )
+}
