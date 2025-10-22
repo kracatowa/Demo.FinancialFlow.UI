89a707722eedf05b66ac89f5fc73aee9ee22e0b8
Ocean Barras <ocean.barras@hotmail.com>
2025-10-22 10:34:10 -0400

    add a sidebar to the main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
 import TopNavbar from './components/TopNavbar'
 import { Outlet } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <TopNavbar />
+      <Sidebar />
       <div className="main-content">
         <Outlet />
       </div>
