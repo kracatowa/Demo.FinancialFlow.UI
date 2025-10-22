8c61254724430d88bfb3d2ef4ffd5d045ada495d
Ocean Barras <ocean.barras@hotmail.com>
2025-10-22 15:15:55 -0400

    small fixes to text and styling of about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,19 +2,19 @@ import "./About.css"
 
 export default function About() {
   return (
-       <div className="about-container">
+       <div className="app-container about-container">
         <h1>À propos</h1>
-        <h2>Description du projet</h2>
+        <h2>Du projet</h2>
         <p>
           Ce projet a été réalisé dans le but de mettre en valeur mes compétences techniques en développement web,
           cloud et automatisation. Il me permet de démontrer ma capacité à concevoir et livrer une application moderne,
-          tout en partageant ma passion pour l’innovation et l’amélioration continue.
+          tout en partageant ma passion pour l'innovation et l'amélioration continue.
         </p>
-        <h2>Océan Barras</h2>
+        <h2>Du développeur</h2>
         <p>
-          Développeur full-stack avec 5 ans d’expérience en .NET et Azure, spécialisé dans la modernisation
-          d’applications financières et l’automatisation de processus métier. Reconnu pour ma rigueur et mon
-          autonomie dans la livraison de solutions complètes, de la conception jusqu’au déploiement en production
+          Océan est un développeur full-stack avec 5 ans d'expérience en .NET et Azure, spécialisé dans la modernisation
+          d'applications financières et l'automatisation de processus métier. Reconnu pour ma rigueur et mon
+          autonomie dans la livraison de solutions complètes, de la conception jusqu'au déploiement en production
           grâce à des pipelines CI/CD.
         </p>
         <hr />
