34e897edc2a2ef7837f05a87f0354c219397e5eb
Ocean Barras <ocean.barras@hotmail.com>
2025-10-22 09:30:14 -0400

    change login to trigger on button

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,18 @@ import Home from './pages/Home'
 import About from './pages/About'
 import { MsalProvider } from "@azure/msal-react";
 import { msalInstance } from "./components/login/AuthConfig";
-import RequireAuth from './components/login/RequiredAuth'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <MsalProvider instance={msalInstance}>
-        <RequireAuth >
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
           </Route>
         </Routes>
-        </RequireAuth>
       </MsalProvider>
     </BrowserRouter>
   </StrictMode>,
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import "./Home.css";
+import { useMsal } from "@azure/msal-react";
+
 export default function Home() {
+  const { instance } = useMsal();
+
+  const handleLogin = () => {
+    instance.loginRedirect();
+  };
+
   return (
-    <div className="">
+    <div className="home-container">
         <h1>Welcome to Financial Flow</h1>
-        <p>Your personal finance management tool.</p>
-        <button>Login</button>
+        <h3>Your personal finance management tool</h3>
+        <p>Login to access the app</p>
+        <button onClick={handleLogin}>Login</button>
     </div>
   )
 }
