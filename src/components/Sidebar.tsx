b092e7e788f7433b22b784379210481d38a7de49
Ocean Barras <ocean.barras@hotmail.com>
2025-10-22 10:33:56 -0400

    reposition bars and update navbar to add file portal route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+import "./Sidebar.css"
+
 export default function Sidebar() {
   return (
-    <div>
-      
+    <div className="sidebar">
+      <h3>Navigation</h3>
+      <ul>
+        <li>
+          <Link to="/file-portal">
+            <button className="sidebar-btn">File Portal</button>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,20 +1,22 @@
 import "./TopNavbar.css"
 import { Link } from "react-router-dom"
+import { FaHome } from "react-icons/fa"
 
 export default function TopNavbar() {
   return (
-    <>
-      <nav className="topnavbar">
-        <div className="topnavbar-logo">Financial Flow</div>
-        <ul className="topnavbar-links">
+    <nav className="topnavbar">
+      <Link to="/" className="topnavbar-logo-link">
+        <span className="home-icon" aria-label="Home" title="Home">
+          <FaHome className="home-icon-svg" />
+        </span>
+        <span className="topnavbar-logo">Financial Flow</span>
+      </Link>
+      <ul className="topnavbar-links">
         <li>
-          <Link to="/">Home</Link>
-        </li>
-          <li>
           <Link to="/about">About</Link>
         </li>
-        </ul>
-      </nav>
-    </>
+      </ul>
+    </nav>
+
   )
 }
