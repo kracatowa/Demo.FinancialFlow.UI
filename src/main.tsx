740ade2cae343b02a96727d4bcff2e3d0af43c9a
Ocean Barras <ocean.barras@hotmail.com>
2025-11-07 15:18:29 -0500

    make a big cleanup to removed unused components

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import './index.css'
 import App from './App'
 import Home from './pages/Home'
 import About from './pages/About'
-import FileTransactions from './pages/FileTransactions'
-import FileAudit from './pages/FileAudit'
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 
@@ -17,20 +15,8 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route
-              path="file-transactions"
-              element={
-                  <FileTransactions />
-              }
-            />
-            <Route
-              path="file-audit"
-              element={
-                  <FileAudit />
-              }
-            />
           </Route>
         </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
